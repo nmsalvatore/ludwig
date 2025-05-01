9a06a2c1c2756290ebe2f14cecd1739a00afd330
Nick Salvatore <nmsalvatore@gmail.com>
2025-05-01 06:47:48 -0700

    tests: add more accounts views tests
    - add registration views tests
    - add failed login redirect test
    - organize credentials in class attributes

--- ludwig/accounts/tests/test_views.py
+++ ludwig/accounts/tests/test_views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user, get_user_model
+from django.contrib.auth import get_user, get_user_model, login
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -10,14 +10,22 @@ class LoginViewTests(TestCase):
         self.register_url = reverse("accounts:register")
         self.login_url = reverse("accounts:login")
         self.dashboard_url = reverse("dashboard:home")
+        self.good_login_credentials = {
+            "username": "testuser",
+            "password": "somepassword123"
+        }
+        self.bad_login_credentials = {
+            "username": "testuser",
+            "password": "wrongpassword123"
+        }
         self.test_login_user = User.objects.create_user(
             username="testuser",
             email="test@example.com",
-            password="complexpassword123"
+            password="somepassword123"
         )
 
     def test_login_page_loads(self):
-        # make GET request to login url
+        # make GET request to login URL
         response = self.client.get(self.login_url)
 
         # check that response status is 200
@@ -26,44 +34,65 @@ class LoginViewTests(TestCase):
 
     def test_successful_login(self):
         # log in user via POST request
-        response = self.client.post(self.login_url, {
-            "username": "testuser",
-            "password": "complexpassword123"
-        })
+        response = self.client.post(
+            self.login_url,
+            self.good_login_credentials
+        )
 
         # check that user is logged in
         user = get_user(self.client)
         self.assertTrue(user.is_authenticated)
 
+    def test_successful_login_redirect(self):
+        # log in user via POST request
+        response = self.client.post(
+            self.login_url,
+            self.good_login_credentials
+        )
+
+        # check that response redirects to dashboard
+        self.assertRedirects(response, reverse("dashboard:home"))
+
     def test_wrong_password(self):
-        # try to log in user with wrong password
-        response = self.client.post(self.login_url, {
-            "username": "testuser",
-            "password": "complexpassword345"
-        })
+        # try to log in with wrong password
+        response = self.client.post(
+            self.login_url,
+            self.bad_login_credentials
+        )
 
         # check that user is not logged in
         user = get_user(self.client)
         self.assertFalse(user.is_authenticated)
 
+    def test_failed_login_redirect(self):
+        # try to log in with wrong password
+        response = self.client.post(
+            self.login_url,
+            self.bad_login_credentials,
+            follow=True
+        )
+
+        # check that redirect chain is empty
+        self.assertEqual(response.redirect_chain, [])
 
 class LogoutViewTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.login_url = reverse("accounts:login")
         self.logout_url = reverse("accounts:logout")
+        self.login_credentials = {
+            "username": "testuser",
+            "password": "somepassword123"
+        }
         self.test_login_user = User.objects.create_user(
             username="testuser",
             email="test@example.com",
-            password="complexpassword123"
+            password="somepassword123"
         )
 
     def test_successful_logout(self):
         # login in user via POST request
-        response = self.client.post(self.login_url, {
-            "username": "testuser",
-            "password": "complexpassword123"
-        })
+        response = self.client.post(self.login_url, self.login_credentials)
 
         # check that user is logged in
         user = get_user(self.client)
@@ -72,16 +101,13 @@ class LogoutViewTests(TestCase):
         # log out user via POST request
         self.client.post(self.logout_url)
 
-        # get fresh snapshot of user
+        # check that user is not logged in
         user_after_logout = get_user(self.client)
         self.assertFalse(user_after_logout.is_authenticated)
 
     def test_logout_redirect(self):
         # login in user via POST request
-        self.client.post(self.login_url, {
-            "username": "testuser",
-            "password": "complexpassword123"
-        })
+        self.client.post(self.login_url, self.login_credentials)
 
         # verify successful login
         user = get_user(self.client)
@@ -92,3 +118,73 @@ class LogoutViewTests(TestCase):
 
         # check that response redirects to login page
         self.assertRedirects(response, reverse('accounts:login'))
+
+
+class RegisterViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse("accounts:login")
+        self.register_url = reverse("accounts:register")
+        self.good_registration_credentials = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "password1": "somepassword123",
+            "password2": "somepassword123",
+        }
+        self.bad_registration_credentials = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "password1": "somepassword123",
+            "password2": "mismatchedpassword"
+        }
+
+    def test_register_page_loads(self):
+        # make GET request to register URL
+        response = self.client.get(self.register_url)
+
+        # check for successful response status
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("accounts/register.html")
+
+    def test_successful_registration(self):
+        # register user via POST request
+        self.client.post(
+            self.register_url,
+            self.good_registration_credentials
+        )
+
+        # check that user is successfully logged in
+        user = get_user(self.client)
+        self.assertTrue(user.is_authenticated)
+
+    def test_successful_registration_redirect(self):
+        # register user via POST request
+        response = self.client.post(
+            self.register_url,
+            self.good_registration_credentials
+        )
+
+        # check that successful registration redirects to dashboard
+        self.assertRedirects(response, reverse("dashboard:home"))
+
+    def test_failed_registration(self):
+        # register user via POST request
+        response = self.client.post(
+            self.register_url,
+            self.bad_registration_credentials
+        )
+
+        # check that user is not authenticated
+        user = get_user(self.client)
+        self.assertFalse(user.is_authenticated)
+
+    def test_failed_registration_redirect(self):
+        # register user via POST request
+        response = self.client.post(
+            self.register_url,
+            self.bad_registration_credentials,
+            follow=True
+        )
+
+        # check that there is no redirect
+        self.assertEqual(response.redirect_chain, [])
