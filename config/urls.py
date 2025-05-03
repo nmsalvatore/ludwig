c8f975a49f6480c2c8d136697b11c40cee36f83c
Nick Salvatore <nmsalvatore@gmail.com>
2025-05-02 17:55:52 -0700

    refactor: convert core app to base app

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
-
-from .views import home_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("ludwig.accounts.urls")),
-    path("dashboard/", include("ludwig.dashboard.urls")),
-    path("d/", include("ludwig.dialogues.urls")),
-    path("", home_view, name="home")
+    path("", include("ludwig.base.urls")),
 ]
