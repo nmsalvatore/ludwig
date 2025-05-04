c04fa7f5345595b78fa58b887d641c880852d756
Nick Salvatore <nmsalvatore@gmail.com>
2025-05-04 08:48:44 -0700

    feat: add list of user dialogues to dashboard

--- ludwig/dashboard/views.py
+++ ludwig/dashboard/views.py
@@ -1,8 +1,13 @@
+from django.contrib.auth import get_user
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 @login_required
 def dashboard_view(request):
-    """View the user's dashboard"""
-    return render(request, "dashboard/dashboard.html")
+    user = get_user(request)
+    user_dialogues = user.dialogues.all()
+    context = {
+        "user_dialogues": user_dialogues
+    }
+    return render(request, "dashboard/dashboard.html", context)
