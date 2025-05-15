71e1a3e1b90671436450b18fc218d34b0929b49f
Nick Salvatore <nmsalvatore@gmail.com>
2025-05-14 19:38:25 -0700

    refactor: convert dashboard view to class-based view, sort by recent post

--- ludwig/dashboard/urls.py
+++ ludwig/dashboard/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "dashboard"
 
 urlpatterns = [
-    path("", views.dashboard_view, name="home")
+    path("", views.DashboardView.as_view(), name="home")
 ]
--- ludwig/dashboard/views.py
+++ ludwig/dashboard/views.py
@@ -1,13 +1,28 @@
 from django.contrib.auth import get_user
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Max
+from django.views.generic.base import TemplateView
 
 
-@login_required
-def dashboard_view(request):
-    user = get_user(request)
-    user_dialogues = user.dialogues.all()
-    context = {
-        "user_dialogues": user_dialogues
-    }
-    return render(request, "dashboard/dashboard.html", context)
+class DashboardView(LoginRequiredMixin, TemplateView):
+    """
+    Display dashboard and show most recent dialogues.
+    """
+
+    template_name = "dashboard/dashboard.html"
+
+    def _get_recent_user_dialogues(self):
+        """Get recent user dialogues, sorted by most recent post"""
+        user = get_user(self.request)
+        user_dialogues = user.dialogues.annotate(
+            latest_post_date=Max('posts__created_on')
+        ).values(
+            "id", "title", "latest_post_date"
+        ).order_by('-latest_post_date')
+        return user_dialogues
+
+    def get_context_data(self, **kwargs):
+        """Add user's recent dialogues to the view context"""
+        context = super().get_context_data(**kwargs)
+        context["user_dialogues"] = self._get_recent_user_dialogues()
+        return context
