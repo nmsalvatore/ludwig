08c5b9cd96b0ff97b8616c5cb4d925ca79bb5880
Nick Salvatore <nmsalvatore@gmail.com>
2025-05-17 13:02:21 -0700

    feat: remove docker

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,9 +11,6 @@ DATABASES = {
     "default": {
         "ENGINE": env.str("DB_ENGINE"),
         "NAME": env.str("DB_NAME"),
-        "USER": env.str("DB_USER"),
-        "PASSWORD": env.str("DB_PASSWORD"),
         "HOST": env.str("DB_HOST"),
-        "PORT": env.str("DB_PORT"),
     }
 }
