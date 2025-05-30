ec8c95893237fae4173c3caefc69c0128c4f9d0f
Nick Salvatore <nmsalvatore@gmail.com>
2025-05-29 20:38:25 -0700

    refactor: moved dashboard tests to tests directory

--- ludwig/dashboard/tests/test_views.py
+++ ludwig/dashboard/tests/test_views.py
@@ -5,30 +5,53 @@ from django.urls import reverse
 
 
 class DashboardViewTest(TestCase):
-    def setUp(self):
-        User = get_user_model()
+    """
+    Test suite for user dashboard view.
 
+    Tests included:
+        1. Redirect of unauthenticated users
+        2. Page load of authenticated users
+        3. TODO: Dialogue with most recent post should be the first in the list of user dialogues
+        4. TODO: Non-participant dialogues should not show in list of user dialogues
+    """
+
+    def setUp(self):
+        """
+        Initial set up for the testing suite.
+        """
+
+        self.client = Client()
+
+        # Create a test user
+        User = get_user_model()
         user = User.objects.create_user(
             username="testuser",
             email="testuser@example.com",
             password="testpassword123",
         )
 
-        self.client = Client()
+        # Store URLs for reuse
         self.dashboard_url = reverse("dashboard:home")
         self.login_url = reverse("accounts:login")
+
+        # Store login credentials for reuse
         self.good_login_credentials = {
             "username": "testuser",
             "password": "testpassword123",
         }
 
     def test_redirect_for_unauthenticated_user(self):
-        """Unauthenticated users should be redirected to login page"""
+        """
+        Unauthenticated users should be redirected to login page.
+        """
         response = self.client.get(self.dashboard_url, follow=True)
         self.assertRedirects(response, "/auth/login/?next=/dashboard/")
 
     def test_page_load_for_authenticated_user(self):
-        """Dashboard should load for authenticated users"""
+        """
+        Dashboard template should successfully load for authenticated
+        users.
+        """
         self.client.post(self.login_url, self.good_login_credentials)
         response = self.client.get(self.dashboard_url)
         self.assertEqual(response.status_code, 200)
