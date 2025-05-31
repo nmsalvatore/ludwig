901a93d41efe6072b5c0e63303e64d5ce5f16d8c
Nick Salvatore <nmsalvatore@gmail.com>
2025-05-31 16:28:44 -0700

    refactor: organize urls after ruff ugliness

--- ludwig/dialogues/urls.py
+++ ludwig/dialogues/urls.py
@@ -7,18 +7,8 @@ app_name = "dialogues"
 urlpatterns = [
     path("create/", views.CreateDialogueView.as_view(), name="create_dialogue"),
     path("search-users/", views.SearchForUsersView.as_view(), name="search_users"),
-    path(
-        "<str:dialogue_id>/", views.DialogueDetailView.as_view(), name="dialogue_detail"
-    ),
+    path("<str:dialogue_id>/", views.DialogueDetailView.as_view(), name="dialogue_detail"),
     path("polling/<str:dialogue_id>", views.PollingView.as_view(), name="polling"),
-    path(
-        "delete/<str:dialogue_id>",
-        views.DeleteDialogueView.as_view(),
-        name="delete_dialogue",
-    ),
-    path(
-        "toggle-visibility/<str:dialogue_id>",
-        views.ToggleVisibilityView.as_view(),
-        name="toggle_visibility",
-    ),
+    path("delete/<str:dialogue_id>", views.DeleteDialogueView.as_view(), name="delete_dialogue"),
+    path("toggle-visibility/<str:dialogue_id>", views.ToggleVisibilityView.as_view(), name="toggle_visibility"),
 ]
