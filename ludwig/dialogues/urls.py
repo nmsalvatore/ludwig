8bfe73178dc75d1f7649178e970c7e5b60ead63b
Nick Salvatore <nmsalvatore@gmail.com>
2025-06-03 07:14:40 -0700

    refactor: changed polling name to update

--- ludwig/dialogues/urls.py
+++ ludwig/dialogues/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("create/", views.CreateDialogueView.as_view(), name="create_dialogue"),
     path("search-users/", views.SearchForUsersView.as_view(), name="search_users"),
     path("<str:dialogue_id>/", views.DialogueDetailView.as_view(), name="dialogue_detail"),
-    path("polling/<str:dialogue_id>", views.PollingView.as_view(), name="polling"),
+    path("update/<str:dialogue_id>", views.DialogueDetailUpdateView.as_view(), name="dialogue_detail_update"),
     path("delete/<str:dialogue_id>", views.DeleteDialogueView.as_view(), name="delete_dialogue"),
     path("toggle-visibility/<str:dialogue_id>", views.ToggleVisibilityView.as_view(), name="toggle_visibility"),
 ]
